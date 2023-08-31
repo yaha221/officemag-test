bc7c4df82a381e53e83db0d39279b1a295a7305f
Anton Stepanyuck <toha.stepanyuck@gmail.com>
2023-08-31 19:31:21 +0300

    Подключение файлов

---
+++ local/php_interface/init.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once __DIR__ . "/ErrorSender.php";
+
+$arJsConfig = array( 
+    'custom_main' => array( 
+        'js' => '/local/js/main.js', 
+        'css' => '/local/js/main.css', 
+        'rel' => array(), 
+    ) 
+); 
+foreach ($arJsConfig as $ext => $arExt) { 
+    \CJSCore::RegisterExt($ext, $arExt); 
+}
+
+CUtil::InitJSCore(array('custom_main'));
