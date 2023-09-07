cbdb880766b3152e022fb51a79a33d95955917a7
Anton Stepanyuck <toha.stepanyuck@gmail.com>
2023-09-07 21:54:33 +0300

    Страница для поиска по коду

---
+++ select2.php
@@ -0,0 +1,6 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetPageProperty("keywords_inner", "код, товар, корзина");
+$APPLICATION->SetPageProperty("title", "Поиск");
+$APPLICATION->SetTitle("Поиск по коду товара");
+?>Пустая страница<br><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
